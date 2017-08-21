7b8788a693a398c6c16acab30dc7b7b2289f1af5
Lijun Xue <lxue@tesla.com>
2017-08-21 10:37:55 -0700

    temp

--- gan.py
+++ gan.py
@@ -1,11 +1,26 @@
 import numpy as np
 import tensorflow as tf
 
-
-def GAN():
-
-def main():
+from tensorflow.contrib.layers import batch_nomarlization
 
 
-if __name__ == '__main__':
-	main()
+class GAN():
+    def __init__(self):
+        self.Z_dim = 100
+        self.G_w1 = 1024
+        self.G_w2 = 512
+        self.G_w3 = 256
+        self.G_w4 = 128
+        self.G_w5 = 3
+        self.batch_size = 50
+        self.image_shape = image_shape
+        return
+
+    def generator(self):
+        with tf.variable_scope('generator') as scope:
+            
+        return
+
+
+    def discriminator(self):
+        return
